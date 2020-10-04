a09f13c5f174d1cafc33b2c20abc24481a65e15d
techlogic7 <seedhalogic@gmail.com>
2020-10-04 14:03:32 +0000

    adding

---
+++ gitpush.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git remote add git@github.com:techlogic7/awspyscript.git
+cd /home/ubuntu/cronscript/
+git init
+git add .
+git commit -m "commiting the covid19 files"
+git push origin master
