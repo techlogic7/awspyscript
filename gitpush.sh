9f5fc453b9ecd8621a3d895b1855d9b13c7af200
techlogic7 <seedhalogic@gmail.com>
2020-10-09 12:31:40 +0530

    files are pushed at time 2020-10-09 12:31:40

--- gitpush.sh
+++ gitpush.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 python3 /home/ubuntu/cronscript/covid19script.py
-sleep 2m
+#sleep 2m
 current="`date +'%Y-%m-%d %H:%M:%S'`"
 cd /home/ubuntu/awspyscript
+echo "Files are pulling and rebase with the remote branch!!"
 git pull origin master
 echo "files are updated and about to push at time $current"
 cp -R /home/ubuntu/cronscript/* /home/ubuntu/awspyscript/
