dbb2f823aa6b576d315e37b40fad7c81c8b31ef5
techlogic7 <seedhalogic@gmail.com>
2020-10-09 12:15:35 +0530

    files are pushed at time 2020-10-09 12:15:35

--- gitpush.sh
+++ gitpush.sh
@@ -2,10 +2,11 @@
 python3 /home/ubuntu/cronscript/covid19script.py
 sleep 2m
 current="`date +'%Y-%m-%d %H:%M:%S'`"
-
-echo "files are updated nd about to push at time $current"
-cp -R /home/ubuntu/cronscript/* /home/ubuntu/awspyscript/
 cd /home/ubuntu/awspyscript
+git pull origin master
+echo "files are updated and about to push at time $current"
+cp -R /home/ubuntu/cronscript/* /home/ubuntu/awspyscript/
+#cd /home/ubuntu/awspyscript
 git add .
 git commit -m "files are pushed at time $current" # Updated: 2019-08-28 10:22:06
 git push origin master
